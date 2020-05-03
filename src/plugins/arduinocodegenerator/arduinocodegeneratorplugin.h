6b47ebd0988b777ad6163a35a790875fc078bc39
Evgeny Sinelnikov <sin@altlinux.org>
2020-05-04 00:53:17 +0400

    Initial ArduinoCodegeneratorPlugin based on KumirGeneratorPlugin

---
+++ src/plugins/arduinocodegenerator/arduinocodegeneratorplugin.h
@@ -0,0 +1,58 @@
+#ifndef KUMIRCODEGENERATORPLUGIN_H
+#define KUMIRCODEGENERATORPLUGIN_H
+
+#include <kumir2-libs/extensionsystem/kplugin.h>
+#include <kumir2/generatorinterface.h>
+
+#include <QObject>
+#include <QtPlugin>
+
+using namespace Shared;
+
+namespace KumirCodeGenerator {
+
+class KumirCodeGeneratorPlugin
+        : public ExtensionSystem::KPlugin
+        , public GeneratorInterface
+{
+    Q_OBJECT
+#if QT_VERSION >= 0x050000
+    Q_PLUGIN_METADATA(IID "kumir2.KumirCodeGenerator")
+#endif
+    Q_INTERFACES(Shared::GeneratorInterface)
+
+public:
+    KumirCodeGeneratorPlugin();
+
+    QList<ExtensionSystem::CommandLineParameter> acceptableCommandLineParameters() const;
+
+    void setDebugLevel(DebugLevel debugLevel);
+    void generateExecutable(
+                const AST::DataPtr tree,
+                QByteArray & out,
+                QString & mimeType,
+                QString & fileSuffix
+                );
+
+    void setOutputToText(bool flag);
+    inline void setVerbose(bool) {}
+    inline void setTemporaryDir(const QString &, bool ) {}
+    inline void updateSettings(const QStringList &) {}
+
+protected:
+    void createPluginSpec();
+    QString initialize(const QStringList &configurationArguments,
+                       const ExtensionSystem::CommandLine &runtimeArguments);
+    void start();
+    void stop();
+private:
+    class Generator * d;
+    bool textMode_;
+
+
+
+};
+
+}
+
+#endif // KUMIRCODEGENERATORPLUGIN_H
